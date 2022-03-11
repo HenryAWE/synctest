52e2e1af9271d039b8e13a13ec5dd92ff01f8e7e
AWE Henry <mouth11or12@outlook.com>
2022-03-11 17:13:14 +0800

    update

--- message.hpp
+++ message.hpp
@@ -9,7 +9,7 @@ namespace awe
     {
         AWEMSG_SYNC = 0, /* int32 id; uint64 frame */
         AWEMSG_CHAT = 1, /* int32 id; string msg */
-        AWEMSG_PLAYER_STATUS = 2, /* int32 id; int8 status */
+        AWEMSG_PLAYER_STATUS = 2, /* int32 id; int32 player_id; int8 status */
         AWEMSG_GAME_START = 3, /* int32 id; uint32 seed */
         AWEMSG_GAME_STOP = 4 /* int32 id */
     };
